353c60e4ad934ef33252b7ab8d2259e6d459ef5e
Kevin Kelly <kvinklly@gmail.com>
2017-11-11 13:07:04 -0600

    io.emit not working as expected

--- public/js/index.js
+++ public/js/index.js
@@ -2,21 +2,12 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: "Marta",
-    text: "I know. I know."
-  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server')
 });
 
-socket.on('newEmail', function(email) {
-  console.log('New email', email);
-});
-
 socket.on('newMessage', function(message) {
   console.log('New message', message);
-})
+});
--- server/server.js
+++ server/server.js
@@ -14,27 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newEmail', {
-    from: 'carrie@example.com',
-    text: 'Hey. what is going on?',
-    createdAt: 123
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('message', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
-
-  socket.on('createEmail', (newEmail) => {
-    console.log('createEmail', newEmail);
-  });
-
-  socket.emit('newMessage', {
-    from: 'Harold',
-    text: 'I eat everything',
-    createdAt: new Date
-  });
-
-  socket.on('createMessage', (newMessage) => {
-    console.log('createMessage', newMessage);
-    newMessage.createdAt = new Date;
-    socket.emit('newMessage', newMessage);
-  })
 
   socket.on('disconnect', () => {
     console.log('User disconnected');
