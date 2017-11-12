1472a8b1f738b444843f1e92d37419aeccdbb47b
Kevin Kelly <kvinklly@gmail.com>
2017-11-12 10:06:32 -0600

    add geolaction support

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,11 +1,11 @@
 var expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage,generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
     var from = 'Admin';
     var text = 'This is a new message';
-    var now = new Date().getTime()
+    var now = new Date().getTime();
     var message = generateMessage(from, text);
     expect(message.from).toBe(from);
     expect(message.text).toBe(text);
@@ -15,3 +15,19 @@ describe('generateMessage', () => {
     expect(message.createdAt).toBeLessThan(now + 1000);
   });
 });
+
+describe('generateMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Larry';
+    var latitude = 1;
+    var longitude = 3;
+    var now = new Date().getTime();
+    var message = generateLocationMessage(from, latitude, longitude);
+    expect(message.from).toBe(from);
+    expect(message.url).toBe(`https://www.google.com/maps?q=1,3`);
+    expect(message.createdAt).toExist();
+    expect(typeof message.createdAt).toBe('number');
+    expect(message.createdAt).toBeGreaterThan(now - 1);
+    expect(message.createdAt).toBeLessThan(now + 1000);
+  })
+})
